0d379d7a9c98e841648215b187d023aeb938733d
Gábor Horváth <xazax.hun@gmail.com>
2014-04-27 11:40:10 +0200

    Automata generator is rewritten and the compiler now have lower memory footprint.

--- include/automata_generator/automata_generator.hpp
+++ include/automata_generator/automata_generator.hpp
@@ -16,55 +16,34 @@ struct DefaultGetInstancePolicy {
   }
 };
 
-// Check if Dest is composable with any of the functions that are
-// after Dest.
-template <typename Dest, typename Target, typename... Targets>
-struct TryComposeWithAll {
-  typedef typename variadic::push_back<
-      typename TryComposeWithAll<Dest, Targets...>::result,
-      ComposabilityRule<Dest, Target>,
-      ComposabilityRule<Target, Dest> >::type result;
+template <template <typename First, typename Second> class Func,
+          typename Binding>
+struct BindFirst {
+  template <typename SecondArg> using type = Func<Binding, SecondArg>;
 };
 
-// Specialized for Dest == Target
-template <typename Dest, typename... Targets>
-struct TryComposeWithAll<Dest, Dest, Targets...> {
-  typedef typename variadic::push_back<
-      typename TryComposeWithAll<Dest, Targets...>::result,
-      ComposabilityRule<Dest, Dest> >::type result;
+template <template <typename First, typename Second> class Func,
+          typename Binding>
+struct BindSecond {
+  template <typename FirstArg> using type = Func<FirstArg, Binding>;
 };
 
-template <typename Dest, typename Target>
-struct TryComposeWithAll<Dest, Target> {
-  // check if we can compose Dest and Target in any way, i.e. Dest(Target) or
-  // Target(Dest)
+template <typename Type, typename TypeList> struct ComposeOneTypeWithAll;
 
-  typedef typename variadic::list<ComposabilityRule<Dest, Target>,
-                                  ComposabilityRule<Target, Dest> >::type
-  result;
+template <typename Type, typename... Others>
+struct ComposeOneTypeWithAll<Type, variadic::list<Others...> > {
+  using type = typename variadic::map<
+      variadic::list<Others...>,
+      BindFirst<ComposabilityRule, Type>::template type>::type;
 };
 
-// Specialized for Dest == Target
-template <typename Dest> struct TryComposeWithAll<Dest, Dest> {
-  typedef typename variadic::list<ComposabilityRule<Dest, Dest> >::type result;
-};
+template <typename... Elements> struct ComposeAllWithAll {
+  using helper = typename variadic::map<
+      variadic::list<Elements...>,
+      BindSecond<ComposeOneTypeWithAll,
+                 variadic::list<Elements...> >::template type>::type;
 
-// Try to compose all of the possible pairing of the functions.
-template <typename Pair, typename... Pairs> struct AutomataImpl {
-  // this is basically an iteration over 'Pairs', like for(Pair : Pairs)
-
-  // gather all results for 'Pair', i.e. all functions that are composable with
-  // 'Pair'
-  typedef typename TryComposeWithAll<Pair, Pair, Pairs...>::result partial;
-
-  // merge the results for 'Pair' with the results of the elements of 'Pairs'
-  // (recurse)
-  typedef typename variadic::concat<typename AutomataImpl<Pairs...>::result,
-                                    partial>::type result;
-};
-
-template <typename Pair> struct AutomataImpl<Pair> {
-  typedef typename variadic::list<ComposabilityRule<Pair, Pair> >::type result;
+  using type = typename variadic::concat_list<helper>::type;
 };
 
 // 'Pairs' is a pack of (decltype, name) pairs: one such pair represents a
@@ -73,8 +52,8 @@ template <typename... Pairs> struct Automata {
   template <typename... IsComposables>
   using CompRules = ComposedRules<DefaultGetInstancePolicy, IsComposables...>;
 
-  typedef typename AutomataImpl<Pairs...>::result vec;
-  typedef typename variadic::copy_pack<vec, CompRules>::type result;
+  typedef typename ComposeAllWithAll<Pairs...>::type list;
+  typedef typename variadic::copy_pack<list, CompRules>::type result;
 };
 
 template <typename GetInstancePolicy, typename... Pairs>
@@ -82,7 +61,7 @@ struct AutomataWithGetInst {
   template <typename... IsComposables>
   using CompRules = ComposedRules<GetInstancePolicy, IsComposables...>;
 
-  typedef typename AutomataImpl<Pairs...>::result vec;
-  typedef typename variadic::copy_pack<vec, CompRules>::type result;
+  typedef typename ComposeAllWithAll<Pairs...>::type list;
+  typedef typename variadic::copy_pack<list, CompRules>::type result;
 };
 #endif
